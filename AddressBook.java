d653ef45047ea0aba6cb5c7f70196141556fa6ee
OjashviSingh <ojashvisingh365@gmail.com>
2023-02-05 21:03:50 +0530

    Added: Program to edit an existing Contact

--- AddressBook.java
+++ AddressBook.java
@@ -1,66 +1,6 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class AddressBook {
-    private ArrayList<Contact> contacts;
-
-    public AddressBook() {
-        contacts = new ArrayList<>();
-    }
-
-    public void addContact(Contact contact) {
-        contacts.add(contact);
-    }
-
-    public ArrayList<Contact> getContacts() {
-        return contacts;
-    }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        AddressBook addressBook = new AddressBook();
-
-        while (true) {
-            System.out.println("Enter first name (or type 'exit' to stop adding contacts): ");
-            String firstName = sc.nextLine();
-            if (firstName.equals("exit")) {
-                break;
-            }
-            System.out.println("Enter last name: ");
-            String lastName = sc.nextLine();
-            System.out.println("Enter address: ");
-            String address = sc.nextLine();
-            System.out.println("Enter city: ");
-            String city = sc.nextLine();
-            System.out.println("Enter state: ");
-            String state = sc.nextLine();
-            System.out.println("Enter zip: ");
-            int zip = sc.nextInt();
-            sc.nextLine(); // clear buffer
-            System.out.println("Enter phone number: ");
-            long phoneNumber = sc.nextLong();
-            sc.nextLine(); // clear buffer
-            System.out.println("Enter email: ");
-            String email = sc.nextLine();
-
-            Contact contact = new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
-            addressBook.addContact(contact);
-        }
-
-        System.out.println("Contacts in address book: ");
-        for (Contact contact : addressBook.getContacts()) {
-            System.out.println("Name: " + contact.getFirstName() + " " + contact.getLastName());
-            System.out.println("Address: " + contact.getAddress());
-            System.out.println("City: " + contact.getCity());
-            System.out.println("State: " + contact.getState());
-            System.out.println("Zip: " + contact.getZip());
-            System.out.println("Phone Number: " + contact.getPhoneNumber());
-            System.out.println("Email: " + contact.getEmail());
-            System.out.println("--------------");
-        }
-    }
-}
-
 class Contact {
     private String firstName;
     private String lastName;
@@ -80,5 +20,99 @@ class Contact {
         this.zip = zip;
         this.phoneNumber = phoneNumber;
         this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
+
+class AddressBook {
+    private ArrayList<Contact> contacts;
+
+    public AddressBook() {
+        this.contacts = new ArrayList<>();
+    }
+
+    public void addContact(Contact contact) {
+        contacts.add(contact);
+    }
+
+    public void editContact(String firstName, String lastName) {
+        for (Contact contact : contacts) {
+            if (contact.getFirstName().equals(firstName) && contact.getLastName().equals(lastName)) {
+                Scanner sc = new Scanner(System.in);
+
+                System.out.println("Enter new address: ");
+                contact.setAddress(sc.nextLine());
+
+                System.out.println("Enter new city: ");
+                contact.setCity(sc.nextLine());
+
+                System.out.println("Enter new state: ");
+                contact.setState(sc.nextLine());
+
+                System.out.println("Enter new zip: ");
+                contact.setZip(sc.nextInt());
+
+                System.out.println("Enter new phone number: ");
+                contact.setPhoneNumber(sc.nextLong());
+
+                System.out.println("Enter new email: ");
+                contact.setEmail(sc.nextLine());
+            }
+        }
     }
 }
